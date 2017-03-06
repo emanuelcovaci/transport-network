03666d4c2be98f7a3fa2613adc84b7dbd72247bc
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-03-06 11:58:37 +0200

    homepage in progres

--- src/config/settings.py
+++ src/config/settings.py
@@ -68,11 +68,9 @@ TEMPLATES = [
         'OPTIONS': {
             'debug': DEBUG,
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'config.context_processors.template_context',
+            
             ],
         },
     },
