b77851a09ff1154fad1a5f712875a81c0dce0c17
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-03-08 21:32:30 +0200

    post page

--- src/homepages/urls.py
+++ src/homepages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^transport', views.transport,name='transport')
+    url(r'^intern', views.transport_intern,name='transport_intern'),
+    url(r'^adr', views.transport_adr,name='transport_adr'),
+    url(r'^frigorific', views.transport_frigorific,name='transport_frigorific')
 ]
--- src/homepages/views.py
+++ src/homepages/views.py
@@ -5,5 +5,12 @@ from django.shortcuts import render
 def index(request):
     return render(request,'homepages/index.html')
 
-def transport(request):
-    return render(request,'homepages/blog.html')
+def transport_intern(request):
+    return render(request,'homepages/intern.html')
+
+def transport_adr(request):
+    return render(request,'homepages/adr.html')
+
+def transport_frigorific(request):
+    return render(request,'homepages/frigorific.html')
+
